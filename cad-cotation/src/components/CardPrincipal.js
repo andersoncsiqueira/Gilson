c3c71fca46becf828c6156659794b71969bac11b
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2022-08-05 10:31:58 -0300

    all it's ok with fetch data

--- cad-cotation/src/components/CardPrincipal.js
+++ cad-cotation/src/components/CardPrincipal.js
@@ -1,28 +1,39 @@
 import refresh from "../assets/monetae.png"
 import '../CardPrincipal.css';
 
+import {useState,useEffect} from "react"
+
+
+
 const CardPrincipal = () => {
 
-
-const getData = async () => {
+  const [price, setPrice] = useState('4,5')
   const url = "https://economia.awesomeapi.com.br/last/CAD-BRL"
-  const response = await fetch(url)
-  const dataCad = await response.json()
-  const price = (Number(dataCad['CADBRL']['ask'])+Number(dataCad['CADBRL']['bid']))/2
+  useEffect(()=>{
+    
+    const getData = async () => {
+  
+      const response = await fetch(url)
+      const dataCad = await response.json()
+      const price0 = (Number(dataCad['CADBRL']['ask'])+Number(dataCad['CADBRL']['bid']))/2
+      
+      setPrice(price0.toFixed(2))
+    
+    }
+
+    getData()
+  },[])
 
 
-  return <p className="span2">{price}</p>
-
-}
 
 
   return (
     <div className="container">
         <div className="content">
             <span className="span1">Cad</span>
-            {getData()}
+            <span className="span2">{price}</span>
         </div>
-        
+       
         <button><img src={refresh} alt="Imagem simbolizando um 'refresh'" className="imgButton"/></button>
     </div>
   )
