45b89e3da3fd7682ec7111ea3db7de53584811fb
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2022-08-05 13:36:03 -0300

    all it's ok with fetch data

--- cad-cotation/src/components/DivWhats.js
+++ cad-cotation/src/components/DivWhats.js
@@ -4,7 +4,7 @@ import wpp from "../assets/whatsapp.svg"
 const DivWhats = () => {
   return (
     <div className="container-Wpp">
-        <p> Quero recarregar</p>
+        <p className="pWhats"> Quero recarregar</p>
         <img src={wpp} alt="icone do whatsapp" className="wpp"/>
         
     </div>
--- cad-cotation/src/components/Saudation.js
+++ cad-cotation/src/components/Saudation.js
@@ -1,10 +1,33 @@
 import "../Saudation.css"
-
+import {useState} from 'react'
 
 const Saudation = () => {
+
+const [time, setTime] = useState()
+
+
+
+const writeHello = () => {
+  const present = new Date()
+
+    console.log(typeof present.getHours())
+
+  if(present.getHours() > 6 && present.getHours() < 12){
+    return <span data-js="hello">Bom dia</span>
+  } else if (present.getHours() > 12 && present.getHours() < 16) {
+    return <span data-js="hello">Boa tarde</span>
+  } else {
+    return <span data-js="hello">Sem recarga agora,</span>
+  }
+
+  
+}
+
+
+
   return (
     <div className="saudation">
-        <p><span data-js="hello">Bom dia</span>, Gilson!!</p>
+        <p className="pSaudation">{writeHello()}, Gilson!!</p>
     </div>
   )
 }
